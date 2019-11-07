72c5e2b2417e577b4146fb784dd34b919599de44
ryan <ryanjohannes23@gmail.com>
2019-11-07 09:09:45 -0800

    Initial Commit.

---
+++ query_starter.py
@@ -0,0 +1,14 @@
+import response
+import bs4
+import googlesearch
+
+
+def main():
+    print("penis")
+
+    for url in googlesearch.search("bam adebayo", stop=10):
+        print(url)
+
+
+if __name__ == "__main__":
+    main()
